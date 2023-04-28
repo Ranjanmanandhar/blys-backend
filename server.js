25863e64d69106119387c7a529569d91353283e3
Ranjan Manandhar <ranjan@ranjan.local>
2023-04-28 13:50:41 +0545

    init

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+var cors = require('cors')
+
 const app = express()
 
-app.get('/api',(req,res) => {
+var corsOptions = {
+    origin: 'https://blys-frontend.onrender.com/',
+    optionsSuccessStatus: 200 
+}
+
+app.get('/api',cors(corsOptions),(req,res) => {
     res.json({"users":['1','2']})
 })
 
-app.post('/otp/verify',(req,res) => {
+app.post('/otp/verify',cors(corsOptions),(req,res) => {
     return res.json("SUCCESS");
 })
 
