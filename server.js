c8e73e7a5972301af87d95acebd7d4e6a8c218ba
Ranjan Manandhar <ranjan@ranjan.local>
2023-04-28 13:47:29 +0545

    init

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/api',(req,res) => {
     res.json({"users":['1','2']})
 })
 
+app.post('/otp/verify',(req,res) => {
+    return res.json("SUCCESS");
+})
+
 app.listen(50000,() => {console.log("server started at 5000")})
